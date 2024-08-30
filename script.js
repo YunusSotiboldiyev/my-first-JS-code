cbb21a81bf30cde5ddd3cca8cd7237d01fb24d32
Yunus <152853532+YunusSotiboldiyev@users.noreply.github.com>
2024-08-30 07:15:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1-vazifa
+var c = 9+1;
+console.log(c);
+// 2-vazifa
+console.log("Hello, World!");
+// 3-vazifa
+let name = prompt("Ismingiz nima");
+console.log(`Salom, ${name}`);
+// 4-vazifa
+var age = prompt("Yoshingiz");
+var dataBirth = 2024-age;
+console.log(`Yilingiz, ${dataBirth}`);
+// 5-vazifa
+var number1 = prompt("1-sonni kiriting");
+var number2 = prompt("2-sonni kiriting");
+
+if(number1>number2){
+console.log(`katta son,${number1}`);
+}else{
+     console.log(`katta son,${number2}`);
+}
